22e41d612872ebf0a801d7a414e63014a2c99881
Matt Gaidica <matt@gaidi.ca>
2014-03-27 20:46:33 -0400

    pellet needs to be brighter, otherwise need a better algorithm?

--- pelletData.m
+++ pelletData.m
@@ -1,7 +1,5 @@
-function [pelletCenter,pelletBbox] = pelletData(image,pelletCenter)
-    pelletCenter = NaN(1,2);
-    pelletBbox = NaN(1,4);
-    
+function [pelletCenter,pelletBbox] = pelletData(image,pelletCenter,searchRadius)
+  
     % consider allowing 2-3 NaN entries come by as a buffer
     if(isnan(pelletCenter(1)))
        return 
@@ -18,18 +16,25 @@ function [pelletCenter,pelletBbox] = pelletData(image,pelletCenter)
 
     % bound the pellet based on starting position
     boundsMask = zeros(size(h));
-    boundsMask = insertShape(boundsMask,'FilledCircle',[pelletCenter 50],'Color','white');
+    boundsMask = insertShape(boundsMask,'FilledCircle',[pelletCenter searchRadius],'Color','white');
     % doesn't need further masking, too small of an area
     mask = h&rgb2gray(boundsMask);
+    imshow(mask);
     
     % get blob properties
     props = regionprops(mask,'Area','Centroid','BoundingBox');
     if(~isempty(props))
         [maxArea,maxIndex] = max([props.Area]);
         % make sure this is a pellet by windowing area
-        if(maxArea > 100 && maxArea < 1200)
+        if(maxArea > 150 && maxArea < 1200)
             pelletCenter = props(maxIndex).Centroid;
             pelletBbox = props(maxIndex).BoundingBox;
+        else
+            pelletCenter = NaN(1,2);
+            pelletBbox = NaN(1,4);
         end
+    else
+        pelletCenter = NaN(1,2);
+        pelletBbox = NaN(1,4);
     end
 end
