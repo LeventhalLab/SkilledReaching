294bea890d0f52255b33e6d25b05542147ce4b31
vibinp <vibinp@umich.edu>
2015-05-19 16:06:35 -0400

    Identifying trigger frame using HSV mask
    working on summing pixel values in ROI

--- identifyTriggerFrameViaHSVMask.m
+++ identifyTriggerFrameViaHSVMask.m
@@ -1,3 +1,9 @@
 function [triggerFrame] = identifyTriggerFrameViaHSVMask(filepath,filename,side)
 videoFilename = fullfile(filepath,filename);
 x = VideoReader(videoFilename);
+for i = 1:x.numberOfFrames;
+    im = read(x,i);    
+    [BW,maskedRGBImage] = createMaskR0027051314001(im);
+    if side == 'right';
+        sum(BW(85:115),
+end
