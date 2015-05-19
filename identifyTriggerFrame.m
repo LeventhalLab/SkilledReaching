d559106503f3a492334e9d051ecc64b580f1ea1a
dleventh <dleventh@med.umich.edu>
2015-05-19 14:07:49 -0400

    update to identifyTriggerFrame

--- identifyTriggerFrame.m
+++ identifyTriggerFrame.m
@@ -1,4 +1,4 @@
-function triggerFrame = identifyTriggerFrame( video, pawPref, varargin )
+function [triggerFrame,peakFrame] = identifyTriggerFrame( video, pawPref, varargin )
 %
 % INPUTS:
 %   video - a VideoReader object for the relevant video
@@ -6,7 +6,7 @@ function triggerFrame = identifyTriggerFrame( video, pawPref, varargin )
 % VARARGs:
 %   numbgframes - number of frames to use at the beginning of the video to
 %       calculate the background
-%   triggerLevel - minimum mean pixel-by-pixel difference from baseline
+%   firstdiffthreshold - minimum mean pixel-by-pixel difference from baseline
 %       that could indicate a trigger event
 %
 % OUTPUTS:
@@ -18,6 +18,8 @@ frames_before_max = 50;
 BGimg = [];
 histWindow = [60 125];     % window within the grayscale histogram in which
                            % to look for an increase when the paw comes in
+
+firstDiffThreshold = 50;   % threshold for first order difference to consider a reach
 
 ROI_to_find_trigger_frame = [  0030         0570         0120         0095
                                1880         0550         0120         0095];
@@ -31,6 +33,8 @@ for iarg = 1 : 2 : nargin - 2
             BGimg = varargin{iarg + 1};
         case 'histwindow',
             histWindow = varargin{iarg + 1};
+        case 'firstdiffthreshold',
+            firstDiffThreshold = varargin{iarg + 1};
     end
 end
 
@@ -57,12 +61,19 @@ BG_rgt = uint8(BGimg(ROI_to_find_trigger_frame(2,2):ROI_to_find_trigger_frame(2,
                      ROI_to_find_trigger_frame(2,1):ROI_to_find_trigger_frame(2,1) + ROI_to_find_trigger_frame(2,3), :));
 % BG_lft_hsv = rgb2hsv(BG_lft);
 % BG_rgt_hsv = rgb2hsv(BG_rgt);
-[BG_lft_hist,histBins] = imhist(rgb2gray(BG_lft));
+[BG_lft_hist,~] = imhist(rgb2gray(BG_lft));
 [BG_rgt_hist,histBins] = imhist(rgb2gray(BG_rgt));
+
+hist_idx = zeros(1,2);    % start and end indices of histogram 
+hist_idx(1) = find(histBins == histWindow(1));
+hist_idx(2) = find(histBins == histWindow(2));
+
+lftHist_baseline = sum(BG_lft_hist(hist_idx(1):hist_idx(2)));
+rgtHist_baseline = sum(BG_rgt_hist(hist_idx(1):hist_idx(2)));
 figure(1)
 bar(histBins,BG_lft_hist)
 title('background')
-mean_BG_subt_values = zeros(2,numFrames);
+histDiff = zeros(2,numFrames);
 for iFrame = 1 : numFrames
 %     iFrame
     img = read(video, iFrame);
@@ -77,6 +88,12 @@ for iFrame = 1 : numFrames
     
     lft_hist = imhist(lft_mirror_gry);
     rgt_hist = imhist(rgt_mirror_gry);
+    
+    lftHistSum = sum(lft_hist(hist_idx(1):hist_idx(2)));
+    rgtHistSum = sum(rgt_hist(hist_idx(1):hist_idx(2)));
+    
+    histDiff(1,iFrame) = lftHistSum - lftHist_baseline;
+    histDiff(2,iFrame) = rgtHistSum - rgtHist_baseline;
 % 	lft_mirror_hsv = rgb2hsv(lft_mirror_img);
 %     rgt_mirror_hsv = rgb2hsv(rgt_mirror_img);
 %     lft_mirror_img = rgb2gray(lft_mirror_img);
@@ -103,21 +120,23 @@ for iFrame = 1 : numFrames
     
 end
 
-
-
+firstDiff = diff(histDiff, 1, 2);
+triggerFrame = find(firstDiff(mirror_idx,:) > firstDiffThreshold, 1, 'first');
+peakFrame = 0;
+% peakFrame = find(
 % find frame with maximum difference between background and current frame
 % in the region of interest
-diffFrame_delta = diff(mean_BG_subt_values(mirror_idx,:));
-maxDiffFrame = find(mean_BG_subt_values(mirror_idx,:) == max(mean_BG_subt_values(mirror_idx,:)));
-maxDeltaFrame = find(diffFrame_delta(maxDiffFrame-frames_before_max:maxDiffFrame) == ...
-                     max(diffFrame_delta(maxDiffFrame-frames_before_max:maxDiffFrame)));
-                 
-% now have frames that are significantly different from 
-% now find the frame with the first significant deviation from baseline
-triggerFrame = maxDeltaFrame + (maxDiffFrame-frames_before_max);
+% diffFrame_delta = diff(mean_BG_subt_values(mirror_idx,:));
+% maxDiffFrame = find(mean_BG_subt_values(mirror_idx,:) == max(mean_BG_subt_values(mirror_idx,:)));
+% maxDeltaFrame = find(diffFrame_delta(maxDiffFrame-frames_before_max:maxDiffFrame) == ...
+%                      max(diffFrame_delta(maxDiffFrame-frames_before_max:maxDiffFrame)));
+%                  
+% % now have frames that are significantly different from 
+% % now find the frame with the first significant deviation from baseline
+% triggerFrame = maxDeltaFrame + (maxDiffFrame-frames_before_max);
 
 figure
-plot(mean_BG_subt_values(1,:))
+plot(histDiff(1,:))
 hold on
-plot(mean_BG_subt_values(2,:),'r')
-plot(triggerFrame, mean_BG_subt_values(mirror_idx,triggerFrame),'linestyle','none','marker','*')
+plot(histDiff(2,:),'r')
+plot(triggerFrame, histDiff(mirror_idx,triggerFrame),'linestyle','none','marker','*')
