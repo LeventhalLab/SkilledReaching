9db76a725f9cbbb9c39074ceab348b8181c5c29c
vibinp <vibinp@umich.edu>
2015-05-21 14:39:51 -0400

    Working on identifying trigger frame using HSV

--- TestHSVFilterVKP.m
+++ TestHSVFilterVKP.m
@@ -22,20 +22,43 @@ for i = 1:length(hsv_imR0027_051314);
     DKLmask{i} = HSVthreshold(hsv_imR0027_051314{i},[.40,.03,.29,.54,.25,.50]);
 end
 
-for i = 1: length(DKLmask)
-    figure;
-    imshow(DKLmask{i})
+% Have it calculate sum of pixel values in ROI for each output from DKLmask
+ROI_to_find_trigger_frame = [  0030         0570         0120         0095
+    1880         0550         0120         0095];
+
+maskScoreLeft = zeros(1,length(DKLmask));
+maskScoreRight = zeros(1,length(DKLmask));
+for i = 1:length(DKLmask)
+    maskScoreLeft(i) = sum(sum(uint8(DKLmask{i}(ROI_to_find_trigger_frame(1,2):ROI_to_find_trigger_frame(1,2) + ROI_to_find_trigger_frame(1,4), ...
+        ROI_to_find_trigger_frame(1,1):ROI_to_find_trigger_frame(1,1) + ROI_to_find_trigger_frame(1,3), :))));
+    maskScoreRight(i) = sum(sum(uint8(DKLmask{i}(ROI_to_find_trigger_frame(2,2):ROI_to_find_trigger_frame(2,2) + ROI_to_find_trigger_frame(2,4), ...
+        ROI_to_find_trigger_frame(2,1):ROI_to_find_trigger_frame(2,1) + ROI_to_find_trigger_frame(2,3), :))));
 end
+plot(maskScoreLeft,'b');
+hold on;
+plot(maskScoreRight,'r');
+hold off;
+
+    
+
+% Plot these sums to determine start frame
+
+
+% for i = 6:16 %1:length(DKLmask)
+%     figure;
+%     imshow(DKLmask{i})
+% end
 %% Attempting to make video from masked images
-% outputVideo = VideoWriter(fullfile('C:\Users\Administrator\Documents\GitHub\SkilledReaching','R0027_051314_DKLmaskVideo.avi'));
+% outputVideo = VideoWriter('R0027_051314_DKLmaskVideo');
 % outputVideo.FrameRate = R0027051314001.FrameRate;
 % open(outputVideo)
+% % cell2struct(DKLmask,'Frame',1);
 % for i = 1:length(DKLmask);
-%     imwrite(DKLmask{i},fullfile('C:\Users\Administrator\Documents\GitHub\SkilledReaching','R0027_051314_DKLmask',i.jpeg));
+%     imwrite(DKLmask{i},fullfile('C:\Users\Administrator\Documents\GitHub\SkilledReaching',['R0027_051314_DKLmask' sprintf('%d',i) '.bmp']));
 % end
 %     
 % for ii = 1:length(DKLmask)
-%    img = DKLmask{ii};
+%    img = imread(fullfile('C:\Users\Administrator\Documents\GitHub\SkilledReaching',['R0027_051314_DKLmask' sprintf('%d',i) '.bmp']));
 %    writeVideo(outputVideo,img)
 % end
 % close(outputVideo)
