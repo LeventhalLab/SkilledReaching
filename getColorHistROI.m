3eef050367fe3aa27c9008a31bf5e7101928d8f8
DJELLENS <damien.j.ellens@gmail.com>
2015-05-26 12:40:31 -0400

    works

--- getColorHistROI.m
+++ getColorHistROI.m
@@ -1,21 +1,28 @@
 %dellens@umich.edu
 
-%beta
-%display color histograms of RGB images
+%display color histograms of ROI from RGB images
+%NB: ROI coordinates are hard-coded, line 16
 %imhist displays a histogram of a grayscale or binary images
 
-function imageX = getColorHist(fname);
-imageX = fname;
+function [imageX] = getColorHist(imageName);
+imageX = imageName;
 
-%show image 'fname' in new figure window
+%show image 'fname' in new figure window, with rectangle overlay for ROI,
+%imrect uses [xmin, ymin, width, height]
 imName = inputname(1);
 figtitle = strcat('Source Image:', imName);
-figure('name', figtitle), imshow(fname);
+figure('name', figtitle), imshow(imageX);
+I = imrect(gca,[120 610 50 50]);
+
+%show cropped image
+position = getPosition(I)
+croppedImageX = imcrop(imageX, position);
+figure('name', figtitle); imshow(croppedImageX);
 
 %split into RGB Channels
-Red = imageX(:,:,1);
-Green = imageX(:,:,2);
-Blue = imageX(:,:,3);
+Red = croppedImageX(:,:,1);
+Green = croppedImageX(:,:,2);
+Blue = croppedImageX(:,:,3);
 
 %Get histValues for each channel
 [yRed, x] = imhist(Red);
