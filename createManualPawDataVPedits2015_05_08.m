3da46c0ef420e208a810de2af1c7ce953278d27b
vibinp <vibinp@umich.edu>
2015-05-27 14:24:28 -0400

    More changes to master manual paw data script
    - removed date field in RatData structure
    - Added cell array for determining if dominant paw used or not
    - Added AutomaticTriggerFrame and AutomaticPeakFrame variables to
    RatData.Videos, which will be filled by results of identifyTriggerFrame
    - If ROI produces peak frame > 5 away from manually determined start
    frame, it will prompt the user to change the ROI by re-drawing a
    rectangle in the appropriate mirror. ROI changes are carried over to
    subsequent trials, based on the assumption that over time it will be
    closest to the optimal ROI. Resets for every day of trials.
    - Paw preference changes according to what is documented in rat's .csv
    file on NAS
    - Added fields to RatData.VideoFiles to output ROI used and paw
    preference. Accuracy also calculated for date overall (automatically
    determined peak frame considered accurate if <5 away from manually
    determined start frame) and displayed in RatData structure.

--- createManualPawDataVPedits2015_05_08.m
+++ createManualPawDataVPedits2015_05_08.m
@@ -1,18 +1,18 @@
 %% Create Fine Motor Movement Data
-% Script guides user in placing specific markers on rat's paw and records 
-% their position in 8 consecutive frames, equally spaced within the 40 
-% frames that follow the start frame. Uses raw video recordings of trial 
-% made each day, and proceeds until all trials for all days are marked for 
+% Script guides user in placing specific markers on rat's paw and records
+% their position in 8 consecutive frames, equally spaced within the 40
+% frames that follow the start frame. Uses raw video recordings of trial
+% made each day, and proceeds until all trials for all days are marked for
 % the chosen rat.
-% 
+%
 %% Input
 % * Rat's raw data folder (selected during script execution)
 % * Start frame (also selected during script execution, defined as the
 % first frame in which the rat's complete paw has passed the slot)
-% 
+%
 %% Output
 % * RatData: Structure array containing two fields
-% 
+%
 % # DateFolders: Listing of the paths of all date folders in rat's raw data
 % folder
 % # VideoFiles: Structural array containing info about all video files
@@ -20,8 +20,8 @@
 % isdir, and datenum. Only the name data is used in this script.
 
 %% Open rat's raw data folder
-clc; 
-clear; 
+clc;
+clear;
 uiwait(msgbox('Please select the rat''s raw data folder','modal'));
 RatDir = uigetdir('\\172.20.138.143\RecordingsLeventhal04\SkilledReaching');
 RatLookUp = dir(RatDir);
@@ -32,78 +32,119 @@ RatID = pathstr(end-4:end);
 RatData().DateFolders = zeros(length(RatLookUp)-3);
 RatData().VideoFiles = zeros(length(RatLookUp)-3);
 for i = 1:(length(RatLookUp)-3)
-     RatData(i).DateFolders = fullfile(RatDir,RatLookUp(i+3).name);  
-     RatData(i).VideoFiles = dir(fullfile(RatData(i).DateFolders,'*.avi'));
-     RatData(i).Date = datetime(RatData(i).DateFolders(end-8:end-1),'InputFormat','yyyyMMdd','Format','MM/dd/yy');     
+    RatData(i).DateFolders = fullfile(RatDir,RatLookUp(i+3).name);
+    RatData(i).VideoFiles = dir(fullfile(RatData(i).DateFolders,'*.avi'));
+    %     for j = 1:(length(RatData(i).VideoFiles));
+    %         video = fullfile(RatData(i).DateFolders,RatData(i).VideoFiles(j).name);
+    %         RatData(i).VideoFiles(j).Object = VideoReader(video);
+    %     end
+    %  Using code commented out above causes following error: Caught
+    %  "std::exception" Exception message is: Message Catalog MATLAB:services was not loaded from the file. Please check file location, format or contents
+    %  And you cannot open variables from the workspace. Looked online and it
+    %  said this is likely because Matlab is trying to open too many files at
+    %  once.
+end
+
+for j = 1:(length(RatData(27).VideoFiles));
+    video = fullfile(RatData(27).DateFolders,RatData(27).VideoFiles(j).name);
+    RatData(27).VideoFiles(j).Object = VideoReader(video);
 end
 
 %% Load video file, determine start frame. Save start frames externally.
-for i = 27:27; %[27 38]; %1: length(RatData);
-    for j = 1:1; %1:length(RatData(i).VideoFiles);        
-        CSVfile = dir(fullfile(pathstr,[sprintf('%s',RatID) '-processed'],sprintf('%s',RatData(i).DateFolders(end-14:end)),'*.csv'));
-        cd(fullfile(pathstr,[sprintf('%s',RatID) '-processed'],sprintf('%s',RatData(i).DateFolders(end-14:end))));
-        CSVfile = xlsread(CSVfile(2).name);
-        RatData(i).VideoFiles(j).ManualStartFrames = CSVfile(j,3);
-        cd('C:\Users\Administrator\Documents\GitHub\SkilledReaching');
-        video = fullfile(RatData(i).DateFolders,RatData(i).VideoFiles(j).name);
-        RatData(i).VideoFiles(j).Object = VideoReader(video);
-        if j == 1 && isnan(RatData(i).VideoFiles(j).ManualStartFrames) == 0;
-            pawpref = input(sprintf('Please input paw preference for video file \n%s: ',video));
+%for i = [27 38]; %1: length(RatData);
+i = 27;
+    CSVfile = dir(fullfile(pathstr,[sprintf('%s',RatID) '-processed'],sprintf('%s',RatData(i).DateFolders(end-14:end)),'*.csv'));
+    cd(fullfile(pathstr,[sprintf('%s',RatID) '-processed'],sprintf('%s',RatData(i).DateFolders(end-14:end))));
+    fileID = fopen(CSVfile(2).name,'r');
+    formatSpec = '%d %*d %d %s';
+    CSVfile = textscan(fileID,formatSpec,'HeaderLines',1,'Delimiter',',','CollectOutput',1);
+    DomPawOrNo = cell(CSVfile{1,2});
+    AutomaticTriggerFrame = zeros(length(RatData(i).VideoFiles),1);
+    AutomaticPeakFrame = zeros(length(RatData(i).VideoFiles),1);
+    cd('C:\Users\Administrator\Documents\GitHub\SkilledReaching');
+    meta_ROI_to_find_trigger_frame = [0030         0570         0120         0095
+        1880         0550         0120         0095];
+    for j = 1:length(RatData(i).VideoFiles);
+        fprintf('Working on trial %d out of %d\n',j,length(RatData(i).VideoFiles));
+        RatData(i).VideoFiles(j).ManualStartFrame = CSVfile{1,1}(j,2);
+        if RatData(i).VideoFiles(j).ManualStartFrame == 0;
+            RatData(i).VideoFiles(j).ManualStartFrame = NaN;
         end
-        if exist('CSVfile(j,4)','var') == 1
-            if strcmp(CSVfile(j,4),'non-dominant paw') == 1;
-                if strcmpi(pawpref,'right') == 1;
+        if j == 1 && (isnan(RatData(i).VideoFiles(j).ManualStartFrame) == 0);
+            video = strrep(fullfile(RatData(i).DateFolders,RatData(i).VideoFiles(j).name),'\','\\');
+            pawpref_master = input(sprintf('Please input paw preference for video file \n%s: ',video));
+            video = fullfile(RatData(i).DateFolders,RatData(i).VideoFiles(j).name);
+        end
+        if ischar(DomPawOrNo{j}) && (isempty(DomPawOrNo{j})==0);
+            if strcmp(DomPawOrNo{j},'non-dominant paw') == 1;
+                if strcmpi(pawpref_master,'right') == 1;
                     pawpref = 'left';
-                elseif strcmpi(pawpref,'left') == 1;
+                elseif strcmpi(pawpref_master,'left') == 1;
                     pawpref = 'right';
                 end
-            else            
+            else
+                pawpref = pawpref_master;                
             end
         else
+            pawpref = pawpref_master;
         end
-        AutomaticStartFrame = zeros(length(RatData(i).VideoFiles),1);
-        [AutomaticStartFrame(i,j), ~] = identifyTriggerFrame(RatData(i).VideoFiles(j).Object,pawpref);
-        RatData(i).VideoFiles(j).AutomaticStartFrame = AutomaticStartFrame(i,j);
-        while abs(RatData(i).VideoFiles(j).AutomaticStartFrame - RatData(i).VideoFiles(j).ManualStartFrames) > 5;
-            im = read(RatData(i).VideoFiles(j).Object,RatData(i).VideoFiles(j).ManualStartFrames);
-            ROI_to_find_trigger_frame = zeros(2,4);
-            if strcmpi(pawpref,'right') == 1; 
-            uiwait(msgbox(sprintf('There is a significant discrepancy between the automatically determined start frame (%d)\nand the manually determined start frame (%d).\nPlease draw a rectangle over the correct ROI in the following image.\nThe ROI should be located in the left mirror',...
-                RatData(i).VideoFiles(j).AutomaticStartFrame,...
-                RatData(i).VideoFiles(j).ManualStartFrames,'modal')));
-            imshow(im);
-            ROI_to_find_trigger_frame(1,:) = getrect;
-            elseif strcmpi(pawpref,'left') == 1; 
-            uiwait(msgbox(sprintf('There is a significant discrepancy between the automatically determined start frame (%d)\nand the manually determined start frame (%d).\nPlease draw a rectangle over the correct ROI in the following image.\nThe ROI should be located in the right mirror',...
-                RatData(i).VideoFiles(j).AutomaticStartFrame,...
-                RatData(i).VideoFiles(j).ManualStartFrames,'modal')));
-            imshow(im);
-            ROI_to_find_trigger_frame(2,:) = getrect;
-            close; 
-            end         
-        ROI_to_find_trigger_frame = round(ROI_to_find_trigger_frame);
-        [AutomaticStartFrame(i,j), ~] = identifyTriggerFrame(RatData(i).VideoFiles(j).Object,pawpref,'trigger_roi',ROI_to_find_trigger_frame);
-        RatData(i).VideoFiles(j).AutomaticStartFrame = AutomaticStartFrame(i,j);
-        end            
-%         StartFrame(i,j) = GUIcreateFrameStartVP2(RatData,i,j,RatDir,RatLookUp,RatID);        
+        
+        [AutomaticTriggerFrame(j), AutomaticPeakFrame(j)] = identifyTriggerFrame(RatData(i).VideoFiles(j).Object,pawpref,'trigger_roi',meta_ROI_to_find_trigger_frame);
+        RatData(i).VideoFiles(j).AutomaticTriggerFrame = AutomaticTriggerFrame(j);
+        RatData(i).VideoFiles(j).AutomaticPeakFrame = AutomaticPeakFrame(j);
+        k = 1;
+        RatData(i).VideoFiles(j).Agree = 1;
+        while abs(RatData(i).VideoFiles(j).AutomaticPeakFrame - RatData(i).VideoFiles(j).ManualStartFrame) > 5;
+            im = read(RatData(i).VideoFiles(j).Object,RatData(i).VideoFiles(j).ManualStartFrame);
+            if k == 3;
+                RatData(i).VideoFiles(j).Agree = 0;
+                break                
+            end
+            if strcmpi(pawpref,'right') == 1;
+                dbstop createManualPawDataVPedits2015_05_08.m;
+                uiwait(msgbox(sprintf('There is a significant discrepancy between the automatically determined start frame (%d) and the manually determined start frame (%d).\nPlease draw a rectangle over the correct ROI in the following image.\nThe ROI should be located in the left mirror',...
+                    RatData(i).VideoFiles(j).AutomaticPeakFrame,...
+                    RatData(i).VideoFiles(j).ManualStartFrame),'modal'));
+                imshow(im);
+                meta_ROI_to_find_trigger_frame(1,:) = getrect;
+                close;
+                k = k+1;
+            elseif strcmpi(pawpref,'left') == 1;
+                dbstop createManualPawDataVPedits2015_05_08.m;
+                uiwait(msgbox(sprintf('There is a significant discrepancy between the automatically determined start frame (%d) and the manually determined start frame (%d).\nPlease draw a rectangle over the correct ROI in the following image.\nThe ROI should be located in the right mirror',...
+                    RatData(i).VideoFiles(j).AutomaticPeakFrame,...
+                    RatData(i).VideoFiles(j).ManualStartFrame),'modal'));
+                imshow(im);
+                meta_ROI_to_find_trigger_frame(2,:) = getrect;
+                close;
+                k = k+1;
+            end
+            meta_ROI_to_find_trigger_frame = round(meta_ROI_to_find_trigger_frame);
+            [AutomaticTriggerFrame(j), AutomaticPeakFrame(j)] = identifyTriggerFrame(RatData(i).VideoFiles(j).Object,pawpref,'trigger_roi',meta_ROI_to_find_trigger_frame);
+            RatData(i).VideoFiles(j).AutomaticTriggerFrame = AutomaticTriggerFrame(j);
+            RatData(i).VideoFiles(j).AutomaticPeakFrame = AutomaticPeakFrame(j);
+        end
+        RatData(i).VideoFiles(j).ROI_Used = meta_ROI_to_find_trigger_frame;
+        RatData(i).VideoFiles(j).Paw_Preference = pawpref;
+        RatData(i).Accuracy = 100.*((sum(RatData(i).VideoFiles(1).Agree:RatData(i).VideoFiles(j).Agree))./j);
+        %         StartFrame(i,j) = GUIcreateFrameStartVP2(RatData,i,j,RatDir,RatLookUp,RatID);
         % Load video, save video data(frames, frame rate, height, width, etc.), and play it
-%         vidObj = VideoReader(fullfile(RatData(i).DateFolders,RatData(i).VideoFiles(j).name));
-%         vidHeight = vidObj.Height;
-%         vidWidth = vidObj.Width;
-%         s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
-%             'colormap',[]);
-%         k = 1;
-%         while hasFrame(vidObj)
-%             s(k).cdata = readFrame(vidObj);
-%             k = k+1;
-%         end
-%         hf = figure;
-%         set(hf,'position',[0 0 vidObj.Width vidObj.Height]);
-%         set(gca,'units','pixels');
-%         set(gca,'position',[0 0 vidObj.Width vidObj.Height]);
-%         movie(hf,s,1,vidObj.FrameRate);
+        %         vidObj = VideoReader(fullfile(RatData(i).DateFolders,RatData(i).VideoFiles(j).name));
+        %         vidHeight = vidObj.Height;
+        %         vidWidth = vidObj.Width;
+        %         s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
+        %             'colormap',[]);
+        %         k = 1;
+        %         while hasFrame(vidObj)
+        %             s(k).cdata = readFrame(vidObj);
+        %             k = k+1;
+        %         end
+        %         hf = figure;
+        %         set(hf,'position',[0 0 vidObj.Width vidObj.Height]);
+        %         set(gca,'units','pixels');
+        %         set(gca,'position',[0 0 vidObj.Width vidObj.Height]);
+        %         movie(hf,s,1,vidObj.FrameRate);
     end
-end
 
 %% Start marking function. Display dialog box indicating which marker and option for indicating not visible and instructions.
 
@@ -119,38 +160,38 @@ end
 % If right click is made after placing marker, prompt user to redo last
 % marker
 
-    % If user selects Yes, display last marker placement dialog box, go
-    % back 1 in marking function and overwrite marker position data.
-    
-    % If user selects No, continue with marker placement.
-    
+% If user selects Yes, display last marker placement dialog box, go
+% back 1 in marking function and overwrite marker position data.
+
+% If user selects No, continue with marker placement.
+
 %% Save marker position data externally
 
 
 %% Old code
 % %% Reset
-% close all 
+% close all
 % clear all
 % clc
-% 
+%
 % %% Pick of the dates from the processed folder
 % disp('Select the directory for which you want to selec the manual points');
 % RatDir = uigetdir('\\172.20.138.142\RecordingsLeventhal3\SkilledReaching');
-% 
-% %% Begin Circling Through Data 
+%
+% %% Begin Circling Through Data
 % RatLookUp = dir(fullfile(RatDir));
 % dates = {RatLookUp(3:end).name}.';%This pulls all the dates in the directory
-% 
-% 
-% 
-% 
+%
+%
+%
+%
 %     for i = 1:length(dates)
-%         
-%  
+%
+%
 %       videoFile = char(fullfile(RatDir,dates(i)));
 %       [pathstr,name,ext] = fileparts(videoFile);
-%       
-%       frameStart = 560; 
+%
+%       frameStart = 560;
 % %       if i == 2
 % %           frameStart =287;
 % %            elseif i == 3
@@ -189,12 +230,12 @@ end
 % %           frameStart = 280;
 % %           else
 % %           frameStart = 300;
-% %          
+% %
 % %       end
-%       
+%
 %       [pellet_center_x, pellet_center_y,manual_paw_centers,mcp_hulls,mph_hulls,dph_hulls] = createManualPoints (videoFile,frameStart)
 %        mkdir(fullfile(pathstr,'manual_trials'));
 %       save(fullfile(pathstr,'manual_trials',name),'pellet_center_x','pellet_center_y','manual_paw_centers','mcp_hulls','mph_hulls','dph_hulls');
-% 
-%     
+%
+%
 %     end
