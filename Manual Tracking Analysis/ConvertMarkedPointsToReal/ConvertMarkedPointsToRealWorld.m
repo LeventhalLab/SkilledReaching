e6c8635fc5cd10a1749a1c0aae40416d1f544705
titusaj <titusaj@umich.edu>
2015-09-02 09:52:30 -0400

    Triangulation working for marked points

--- Manual Tracking Analysis/ConvertMarkedPointsToReal/ConvertMarkedPointsToRealWorld.m
+++ Manual Tracking Analysis/ConvertMarkedPointsToReal/ConvertMarkedPointsToRealWorld.m
@@ -20,50 +20,42 @@
 % 3.) Normalize the points to homogeneous points
 % 4.) Shoot into triangulate_DL.
 
-function ConvertMarkedPointsToRealWorld
+function   [points3d,reprojectedPoints,errors]= ConvertMarkedPointsToRealWorld
     load('cameraParameters.mat');
     
-    x1 = [973.7055562	635.1111006
-966.035991	571.0154484
-919.4707736	567.7284919
-929.3316432	565.5371875
-945.7664258	571.0154484
-916.1838171	584.1632745
-929.8794693	595.6676223
-947.409904	602.7893615
-918.3751214	602.2415354
-922.209904	611.5545788
-933.1664258	619.2241441
+    x1 = [1101.151996	595.9071792
+1121.499822	601.5593532
+1136.760692	610.6028314
+1078.543301	591.9506575
+1094.369388	605.5158749
+1107.369388	626.4289184
+
 
 ];
 
-    x2= [68.81985942	602.7868894
-106.1068159	564.3347155
-148.8314536	561.6158749
-131.7415985	560.8390633
-111.5444971	562.7810923
-151.9387	570.937614
-130.1879754	575.9868894
-113.4865261	579.4825416
-153.1039174	589.1926865
-146.8894246	588.8042807
-135.6256565	593.4651503
+    x2= [1880.773196	574.8494036
+1911.994127	588.3784733
+1914.075522	602.4278919
+1906.270289	576.9307989
+1934.369127	583.174985
+1947.377848	597.7447524
 ];
 
 %     %Undistort the points
-%     x1 = undistortPoints(x1,cameraParams);
-%     x2 = undistortPoints(x2,cameraParams);
+    x1 = undistortPoints(x1,cameraParams);
+    x2 = undistortPoints(x2,cameraParams);
     
-    %Create homogenous  verions of the points
-    x1 = [x1, ones(size(x1,1),1)]';
-    x2 = [x2, ones(size(x2,1),1)]';
+    %Create homogenous  verions of the points    
+    x1_hom = [x1, ones(size(x1,1),1)]';
+    x2_hom = [x2, ones(size(x2,1),1)]';
+
     
     %Grab the intrsic matrix
     k = cameraParams.IntrinsicMatrix;
   
     
     %Calculate the fundemental matrix
-    F = fundMatrix_mirror(x1, x2);
+    F = fundMatrix_mirror(x1_hom, x2_hom);
     
     %Calculate the essential matric
     E = k * F *k';
@@ -72,7 +64,7 @@ function ConvertMarkedPointsToRealWorld
     [rot,t] = EssentialMatrixToCameraMatrix(E);
     
     %Select correct rot and t 
-    [cRot,cT,correct] = SelectCorrectEssentialCameraMatrix_mirrorTJ(rot,t,x1,x2,k);
+    [cRot,cT,correct] = SelectCorrectEssentialCameraMatrix_mirrorTJ(rot,t,x1_hom,x2_hom,k);
    
     %Create the projection matricies
      P1= eye(4,3);% P1 stays constants 4x3 matrix
@@ -80,7 +72,7 @@ function ConvertMarkedPointsToRealWorld
     
      
      %Use the triangulation function 
-    [points3d,reprojectedPoints,errors] = triangulate_DL(x1', x2', P1, P2)
+    [points3d,reprojectedPoints,errors] = triangulate_DL(x1, x2, P1, P2)
     
 
 end
