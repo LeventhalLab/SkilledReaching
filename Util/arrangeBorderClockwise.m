85a7d9f5a8ecdda4d25bafc8f58bc4dc516b1c3e
dleventh <dleventh@med.umich.edu>
2015-12-14 15:50:31 -0500

    improved algorithm to order points around the perimeter

--- Util/arrangeBorderClockwise.m
+++ Util/arrangeBorderClockwise.m
@@ -2,39 +2,103 @@ function [new_points, idx] = arrangeBorderClockwise(mask_ext)
 
 
 new_points = zeros(size(mask_ext));
-idx = zeros(size(mask_ext,1));
+numPoints  = size(mask_ext,1);
+idx = zeros(numPoints,1);
 
 new_points(1,:) = mask_ext(1,:);
 idx(1) = 1;
 
-for ii = 1 : length(mask_ext) - 1
-    % find the 2 points closest to the current point. one of them will be
-    % the next point, one of them will be the previous point (if we've
-    % gotten that far - meaning ii > 2)
-    xy_diff = bsxfun(@minus, mask_ext, new_points(ii,:));
-    dist_from_current_point = sqrt(sum(xy_diff.^2,2));
-    
-    % find at least the 2 closest points (maybe more if there's a third
-    % point the same distance away as the first two)
-    [sorted_dist, sorted_idx] = sort(dist_from_current_point);
-    poss_idx = sorted_idx(2:3);
-    additional_poss_idx = (sorted_dist(4:end) == sorted_dist(3));
-    if any(additional_poss_idx)
-        poss_idx = [poss_idx; sorted_idx(find(additional_poss_idx)+3)];
+connectivity = 8;
+
+for ii = 1 : numPoints - 1
+    % find all connected points to the current point
+    [~,poss_idx] = findAdjacentPoints(mask_ext, new_points(ii,:), connectivity);
+    poss_idx = find(poss_idx);
+    if ii > 1
+        poss_idx = poss_idx(~ismember(poss_idx,idx(1:ii)));
     end
     
-    % first, eliminate any indices that have already been used
-    
-    % WORKING HERE - NOW NEED TO ELIMINATE THE ONE POINT THAT HAS ALREADY
-    % BEEN USED, THEN PICK BETWEEN THE OTHER TWO SOMEHOW...
-    
-    if any(idx(1:ii) == poss_idx(1))
-        valid_idx = poss_idx(2);
-    else
+    if length(poss_idx) == 1
+        valid_idx = poss_idx;
+    elseif ii == 1
         valid_idx = poss_idx(1);
+    else
+        % what if there are 2 neighboring points that haven't been assigned
+        % yet?
+        
+        valid_idx = selectNextPoint(mask_ext, valid_idx, idx, connectivity);
+        % in above line, valid_idx in the selectNextPoint arguments is from
+        % the previous iteration
+
     end
-    
+
     idx(ii+1) = valid_idx;
     new_points(ii+1,:) = mask_ext(valid_idx,:);
     
 end
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function next_pt_idx = selectNextPoint(mask_ext, current_idx, assigned_idx, connectivity)
+
+foundNextPoint = false;
+% pts_to_exclude = assigned_idx;
+% currentTestPoint = current_idx;
+
+% find all adjacent points to the current point
+[~, poss_idx] = findAdjacentPoints(mask_ext, ...
+                                   mask_ext(current_idx,:), ...
+                                   connectivity);
+poss_idx = find(poss_idx);
+poss_idx = poss_idx(~ismember(poss_idx,assigned_idx));
+
+for ii = 1 : length(poss_idx)
+    % if the current poss_idx point has no adjacent points that either
+    % haven't already been assigned or are another possible next point
+    % around the perimeter, we've found the next point
+
+    [~, next_poss_idx] = findAdjacentPoints(mask_ext, ...
+                                            mask_ext(poss_idx(ii),:), ...
+                                            connectivity);
+	next_poss_idx = find(next_poss_idx);
+    next_poss_idx = next_poss_idx(~ismember(next_poss_idx,[assigned_idx; poss_idx]));
+    if isempty(next_poss_idx)
+        foundNextPoint = true;
+        next_pt_idx = poss_idx(ii);
+    elseif length(next_poss_idx) == 1
+        % if there is exactly one adjacent point, does that point have any
+        % adjacent points that haven't already been accounted for. If not,
+        % then poss_idx(ii) must be the next point around the perimeter
+        [~,next_next_poss_idx] = findAdjacentPoints(mask_ext, ...
+                                                    mask_ext(next_poss_idx,:), ...
+                                                    connectivity);
+        next_next_poss_idx = find(next_next_poss_idx);
+        next_next_poss_idx = next_next_poss_idx(~ismember(next_next_poss_idx,...
+                                                [assigned_idx; poss_idx; next_poss_idx]));
+        if isempty(next_next_poss_idx)
+            foundNextPoint = true;
+            next_pt_idx = poss_idx(ii);
+        end
+    end
+end
+if foundNextPoint; return; end
+
+% all of the potential adjacent points have 2 more potential adjacent
+% points next to them. in that case, pick the possible point that is
+% closest to the current point. 
+
+% NEED TO THINK CAREFULLY ABOUT THIS - WHAT IF THE TWO ADJACENT POINTS ARE
+% ON A DIAGONAL? ONE IDEA IS TO LOOK FOR POINTS THAT LIE ALONG THE SAME
+% LINE?
+
+
+xy_diff_adjacent_points = bsxfun(@minus, mask_ext(poss_idx,:), mask_ext(current_idx,:));
+dist_adjacent_points = sqrt(sum(xy_diff_adjacent_points.^2,2));
+next_pt_idx = poss_idx(dist_adjacent_points == min(dist_adjacent_points));
+
+end
+
