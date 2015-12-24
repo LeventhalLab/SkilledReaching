a087f0e3b005f341bb0f7fc1fc6775024f939378
dleventh <dleventh@med.umich.edu>
2015-12-24 10:59:42 -0500

    working on figure for paper

--- DKL_SR_methods_fig/plot3Dtrajectories.m
+++ DKL_SR_methods_fig/plot3Dtrajectories.m
@@ -3,7 +3,8 @@ function plot3Dtrajectories(x,y,z,varargin)
 slot_z = 175;
 
 h_axes = 0;
-plotCol = 'k';
+indTrialCol = 'k';
+meanCol = 'k';
 fadeColor = false;
 showIndTraj = true;
 showMean = true;
@@ -11,13 +12,18 @@ meanWeight = 2;
 indTrajWeight = 0.5;
 onlyValidFrames = true;
 
+xLimits = [-10 20];
+yLimits = [-10 30];
+zLimits = [160 200];
 for iarg = 1 : 2 : nargin - 3
     
     switch lower(varargin{iarg})
         case 'axes',
             h_axes = varargin{iarg + 1};
-        case 'color',
-            plotCol = varargin{iarg + 1};
+        case 'indtrialcol',
+            indTrialCol = varargin{iarg + 1};
+        case 'meancol',
+            meanCol = varargin{iarg + 1};
         case 'fadecolor',
             fadeColor = varargin{iarg + 1};
         case 'showmean',
@@ -30,6 +36,12 @@ for iarg = 1 : 2 : nargin - 3
             onlyValidFrames = varargin{iarg + 1};
         case 'slot_z',
             slot_z = varargin{iarg + 1};
+        case 'xlim',
+            xLimits = varargin{iarg + 1};
+        case 'ylim',
+            yLimits = varargin{iarg + 1};
+        case 'zlim',
+            zLimits = varargin{iarg + 1};
     end
       
 end   % for iarg...
@@ -53,11 +65,12 @@ if showIndTraj
     for i_plot = 1 : length(x)
 
         plot3(x{i_plot},y{i_plot},z{i_plot},...
-              'color',plotCol,...
+              'color',indTrialCol,...
               'linewidth',indTrajWeight);
         xlabel('x');
         ylabel('y');
         zlabel('z');
+        
         hold on
     end
 end
@@ -67,8 +80,9 @@ if showMean
     plot3(meanTrajectory(validFrames,1),...
           meanTrajectory(validFrames,2),...
           meanTrajectory(validFrames,3),...
-          'color',plotCol, ...
+          'color',meanCol, ...
           'linewidth',meanWeight);
 end
 
+set(gca,'xlim',xLimits,'ylim',yLimits,'zlim',zLimits);
 set(gca,'ydir','reverse')
