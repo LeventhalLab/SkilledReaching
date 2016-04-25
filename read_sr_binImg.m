8d300bbce33fa1a65aab95211d777fd9be7a9b6f
dleventh <dleventh@med.umich.edu>
2016-04-25 15:03:14 -0400

    fixed binary image read

--- read_sr_binImg.m
+++ read_sr_binImg.m
@@ -15,7 +15,10 @@ h = 1024;
 
 for iarg = 1 : 2 : nargin - 2
     switch lower(varargin{iarg + 1});
-        
+        case 'w',
+            w = varargin{iarg + 1};
+        case 'h',
+            h = varargin{iarg + 1};
     end
 end
 
@@ -23,7 +26,10 @@ fid = fopen(fname,'r');
 
 % there is a 4-byte header before each frame
 fseek(fid,4*frame_num + h*(w)*(frame_num-1),'bof');
-img = uint8(fread(fid, [w,h], 'uint8', 0, 'l'))';
+img = (fread(fid, [1,h*w / 8], 'double', 0, 'b'));
+
+b = typecast(img,'uint8');
+img = reshape(b,[w,h])';
 
 fclose(fid);
 
