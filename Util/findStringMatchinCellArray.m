2a5f261c84254cdee86ef56bd8cbef38eaa78350
dleventh <dleventh@umich.edu>
2018-09-05 13:42:31 -0400

    more trajectory mapping

--- Util/findStringMatchinCellArray.m
+++ Util/findStringMatchinCellArray.m
@@ -2,10 +2,10 @@ function match_idx = findStringMatchinCellArray(cell_array, str_fragment)
 
 stringSearch = strfind(cell_array,str_fragment);
 
-match_idx = zeros(length(stringSearch),1);
+match_idx = false(length(stringSearch),1);
 for ii = 1 : length(stringSearch)
     
-    match_idx = ~isempty(stringSearch{ii});
+    match_idx(ii) = ~isempty(stringSearch{ii});
     
 end
 
