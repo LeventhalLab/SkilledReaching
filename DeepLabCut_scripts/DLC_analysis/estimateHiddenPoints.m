47d2e6a4d53e6b9ed73c2e178fdd5d5ba6dc7d63
dleventh <dleventh@umich.edu>
2018-11-21 16:49:25 -0500

    estimate hidden points

--- DeepLabCut_scripts/DLC_analysis/estimateHiddenPoints.m
+++ DeepLabCut_scripts/DLC_analysis/estimateHiddenPoints.m
@@ -1,5 +1,8 @@
 function [final_direct_pts,final_mirror_pts,isEstimate] = estimateHiddenPoints(direct_pts, mirror_pts, direct_p, mirror_p, direct_bp, mirror_bp, boxCal, ROIs, imSize, pawPref)
 
+vidPath = '/Volumes/Tbolt_01/Skilled Reaching/R0186/R0186_20170813a';
+video = VideoReader(fullfile(vidPath,'R0186_20170813_12-09-21_002.avi'));
+
 switch pawPref
     case 'right'
         F = squeeze(boxCal.F(:,:,2));
@@ -39,12 +42,31 @@ for iFrame = 1 : numFrames
         % figure out whether the mirror or direct view point was identified
         if invalid_direct(direct_part_idx,iFrame)
             % the mirror point was identified
-            known_pt = squeeze(final_mirror_pts(mirror_part_idx,iFrame,:));
+            known_pt = squeeze(final_mirror_pts(mirror_part_idx,iFrame,:))';
+            
+            other_pts = squeeze(final_direct_pts(direct_mcp_idx,iFrame,:));
+            other_pts = other_pts(~invalid_direct(direct_mcp_idx,iFrame),:);
         else
-            known_pt = squeeze(final_direct_pts(mirror_part_idx,iFrame,:));
+            known_pt = squeeze(final_direct_pts(mirror_part_idx,iFrame,:))';
+            
+            other_pts = squeeze(final_mirror_pts(mirror_mcp_idx,iFrame,:));
+            other_pts = other_pts(~invalid_mirror(mirror_mcp_idx,iFrame),:);
+        end
+        if isempty(other_pts)
+            continue;   % no nearest point to match with. probably will update this later to allow other points to be used...
         end
         epiLine = epipolarLine(F,known_pt);
         epiBorderPts = lineToBorderPoints(epiLine, imSize);
+        epiBorderPts = [epiBorderPts(1:2);epiBorderPts(3:4)];
+        
+        
+        % find index of other_pts that is closest to the epipolar line
+        [~, nnidx] = findNearestPointToLine(epiBorderPts, other_pts);
+        
+        % find the point on the epipolar line closest to any of the
+        % identified digit points
+        np = findNearestPointOnLine(epiBorderPts,other_pts(nnidx,:));
+            
         % WORKING HERE...
         % FIND PAW POINT IN THE VIEW WHERE THE POINT OF INTEREST WASN'T
         % FOUND THAT'S CLOSEST TO THE EPIPOLAR LINE THAT IS OF THE SAME
@@ -66,7 +88,7 @@ for iFrame = 1 : numFrames
         
     end
 end
-end
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
