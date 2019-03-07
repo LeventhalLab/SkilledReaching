e2895e8c2de2fd721df2909dc55db4b8b10b2b32
dleventh <dleventh@umich.edu>
2019-03-07 17:53:40 -0500

    more processing

--- DeepLabCut_scripts/DLC_analysis/script_calculateKinematics_20190218.m
+++ DeepLabCut_scripts/DLC_analysis/script_calculateKinematics_20190218.m
@@ -88,7 +88,7 @@ cd(labeledBodypartsFolder)
 ratFolders = dir('R*');
 numRatFolders = length(ratFolders);
 
-for i_rat = 15 : 15%numRatFolders
+for i_rat = 4 : 15%numRatFolders
 
     ratID = ratFolders(i_rat).name
     ratIDnum = str2double(ratID(2:end));
--- DeepLabCut_scripts/DLC_analysis/script_plotMeanTrajectories.m
+++ DeepLabCut_scripts/DLC_analysis/script_plotMeanTrajectories.m
@@ -13,8 +13,8 @@ var_lim = [0,5;
            0,10];
 pawFrameLim = [0 400];
 
-skipTrialPlots = true;
-skipSessionSummaryPlots = true;
+skipTrialPlots = false;
+skipSessionSummaryPlots = false;
 
 % paramaeters for readReachScores
 csvDateFormat = 'MM/dd/yyyy';
@@ -120,7 +120,7 @@ ratInfo_IDs = [ratInfo.ratID];
 ratFolders = findRatFolders(labeledBodypartsFolder);
 numRatFolders = length(ratFolders);
 
-for i_rat = 22:22%numRatFolders
+for i_rat = 15:15%numRatFolders
     
     ratID = ratFolders{i_rat};
     ratIDnum = str2double(ratID(2:end));
