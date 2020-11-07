6f04e2b4c0f74f629ea1f22569c495f148e7ff89
dleventh <dleventh@umich.edu>
2020-11-07 07:32:29 -0500

    moved elife repository

--- Bova et al 2020 code repository/utilities/readSessionInfoTable.m
+++
@@ -1,8 +0,0 @@
-function sessionInfo = readSessionInfoTable(csvfname)
-%
-% function to read rat information in from a .csv spreadsheet (birth dates,
-% training dates, interventions, etc.)
-
-sessionInfo = readtable(csvfname);
-
-sessionInfo = cleanUpSessionsTable(sessionInfo);
